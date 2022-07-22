800598cf00e6516c632215b80d4178031eb1b8d8
beydonce <106409904+beydonce@users.noreply.github.com>
2022-07-22 17:44:08 +0300

    Add files via upload

---
+++ game_script.js
@@ -0,0 +1,43 @@
+// function moveFunction() {
+//     const style = document.getElementById('google-Logo').style;
+//     for (let i = 1; i <= 69; i++) {
+//         const currentMargin = parseInt(style.marginLeft) || 0;
+//         style.marginLeft = `${currentMargin + 10}px`;
+//     }
+// }
+
+let count = 0;
+let count2 = 0;
+
+function playAgain() {
+    let randomNumber1 = Math.floor(Math.random() * 6) +1;
+    let randomNumber2 = Math.floor(Math.random() * 6) +1;
+    let randomImages = "/images/dice" + randomNumber1 + ".png";
+    let randomImages2 = "/images/dice" + randomNumber2 + ".png";
+
+    document.querySelectorAll("img")[0].setAttribute("src", randomImages);
+
+    document.querySelectorAll("img")[1].setAttribute("src", randomImages2);
+    
+    if (randomNumber1 > randomNumber2) {
+        document.getElementById("header").innerHTML = "🌟Player 1 Wins!🌟";
+        document.getElementById("countOne").innerText = `${++count}` ;
+    }
+    else if(randomNumber2>randomNumber1) {
+        document.getElementById("header").innerHTML = "🌟Player 2 Wins!🌟";
+        document.getElementById("countTwo").innerText = `${++count2}` ;
+    }
+    else {
+        document.getElementById("header").innerHTML = "Draw !";
+    }
+}
+
+document.getElementById("end").onclick = function(){
+    location.href = "home_screen.html";
+}
+
+
+
+
+
+  
